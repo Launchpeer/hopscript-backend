53e200f79b131c6de1976366e9a712f9386a949c
Lucius <lucius@Luciuss-iMac.local>
2018-05-07 16:28:14 -0400

    fixes typo

--- cloud/routes/agents.js
+++ cloud/routes/agents.js
@@ -7,7 +7,7 @@ Parse.Cloud.define('sendEmailInvite', function(req, res) {
     to: req.params.email,
     from: 'no-reply@swiftscript.com',
     subject: 'Temporary Password',
-    text: `You’ve been invited to use Swift Script by ${req.params.brokerage}. To get started, follow this link https://swiftscript.herokuapp.com/. Login with this email address and your temporary password: ${req.params.password}. To access your account. If you have questions about why you received this email, please contact ${req.params.brokerageEmail}`
+    text: `You’ve been invited to use Swift Script by ${req.params.brokerage}, to get started, follow this link https://swiftscript.herokuapp.com/. Login with this email address and your temporary password: ${req.params.password}. To access your account. If you have questions about why you received this email, please contact ${req.params.brokerageEmail}`
   };
   sgMail.send(msg);
   return res.success("email sent");
