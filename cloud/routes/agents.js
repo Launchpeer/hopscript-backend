2b2d580dc39dcb8b3a96b90cb096a6a8f4db99ad
Lucius <lucius@Luciuss-iMac.local>
2018-05-07 16:15:55 -0400

    updates from email to swift script

--- cloud/routes/agents.js
+++ cloud/routes/agents.js
@@ -5,7 +5,7 @@ Parse.Cloud.define('sendEmailInvite', function(req, res) {
   sgMail.setApiKey(config.SENDGRID_API_KEY);
   const msg = {
     to: req.params.email,
-    from: 'no-reply@lgnd.com',
+    from: 'no-reply@swiftscript.com',
     subject: 'Temporary Password',
     text: `You’ve been invited to use Swift Script by ${req.params.brokerage}. To get started, follow this link https://swiftscript.herokuapp.com/. Login with this email address and your temporary password: ${req.params.password}. To access your account. If you have questions about why you received this email, please contact ${req.params.brokerageEmail}`
   };
