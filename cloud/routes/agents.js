915feb3ca706bd656d4bfcfd6c835b5e51ca593d
Lucius <lucius@Luciuss-iMac.local>
2018-05-08 11:06:05 -0400

    adds documentation

--- cloud/routes/agents.js
+++ cloud/routes/agents.js
@@ -2,6 +2,19 @@ const config = require('../../config');
 const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey(config.SENDGRID_API_KEY);
 
+/**
+ * As a broker I want to send an agent an invite email
+
+ A sendgrid message object is instantiated and sent to the agent.
+
+ Loading and Errors are handled for UX
+
+ * @param  {string} email email address of Agent
+ * @param  {string} brokerage username of Brokerage that is inviting the Agent
+ * @param  {string} password password generated for the Agent
+ * @param  {string} brokerageEmail email address of the Brokerage
+ */
+
 Parse.Cloud.define('sendEmailInvite', function(req, res) {
   const msg = {
     to: req.params.email,
