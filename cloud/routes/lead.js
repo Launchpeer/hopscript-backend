4b909732e1f6468e6398bfc79f9c7b5f2c5d108b
Kayla Kantola <krkantola@gmail.com>
2018-06-16 10:54:41 -0400

    adds leadgroup functions

--- cloud/routes/lead.js
+++ cloud/routes/lead.js
@@ -1,6 +1,28 @@
 /**
- * As an agent I want to create a Lead
-
+ * As an agent I want to create a Lead.
+ *
+ * On Submit of the create lead form, a Lead object is passed to this function as req.params.lead.
+ * The object contains a name, phone, email, leadType, and leadGroup.
+ * We also have access to the user (Agent) off of req.user.
+ *
+ * First we destructure Lead off of req.params, destructure Agent off of req.user,
+ * establish a new Lead object, and set up formatting for the phone number to work with Twilio's API.
+ *
+ * Then, we query for the LeadGroup that the lead has been assigned to (lead.leadGroup is the leadgroup's ID).
+ * Once we have the leadgroup, we start setting items on the Lead object. We have to use "addUnique"
+ * on the leadGroup so that it is passed in as one pointer in what will be an array of pointers.
+ * We save the Lead object.
+ *
+ * Then, we take the newly saved lead, and add it as a pointer in an array of pointers on the LeadGroup.
+ * We save the LeadGroup.
+ *
+ * Then, we query for the user making the request using req.user.id, using useMasterKey.
+ * We add the newly saved lead as a pointer in an array of pointers on the User.
+ * We save the user, using useMasterKey.
+ *
+ *  Loading and Errors are handled for UX.
+ *
+ * @param  {object} lead An object representing the lead, including a name, phone, email, leadType, and leadGroup
  *
  */
 
@@ -9,7 +31,6 @@ Parse.Cloud.define('createLead', (req, res) => {
   const Agent = req.user;
   const LObj = new Parse.Object('Lead');
   const formattedPhone = `+1${lead.phone}`;
-  // Get the leadGroup pointer object from Parse
   const leadGroupQuery = new Parse.Query("LeadGroup");
   leadGroupQuery.get(lead.leadGroup)
     .then((leadGroup) => {
@@ -21,7 +42,6 @@ Parse.Cloud.define('createLead', (req, res) => {
       LObj.set('agent', Agent);
       LObj.save()
         .then((newlySavedLead) => {
-          console.log("newlySavedLead", newlySavedLead.attributes);
           leadGroup.addUnique("leads", newlySavedLead);
           leadGroup.save()
             .then(() => {
@@ -41,15 +61,54 @@ Parse.Cloud.define('createLead', (req, res) => {
 
 
 /**
+ * As an agent I want to fetch a Lead
+ *
+ * We query the database for Lead
+ * If found, we return the Lead & its associated leadGroups.
+ *
+ * @param  {object} lead An object representing the lead, including an id, name, phone, email, leadType, and leadGroup
+ *
+ */
+
+Parse.Cloud.define('fetchLead', (req, res) => {
+  const leadQuery = new Parse.Query('Lead');
+  leadQuery.include('leadGroups');
+  leadQuery.get(req.params.lead)
+    .then(lead => res.success(lead))
+    .catch(err => res.error(err));
+});
+
+/**
+ * As an agent I want to fetch my Leads
+ * We query the database for Leads, checking to see if the lead's Agent is equal to the user making the query.
+ * If found, we return the Leads.
+ *
+ *@param  {object} user An object representing the user, including their leads
+ *
+ */
+Parse.Cloud.define('fetchLeads', (req, res) => {
+  const leadsQuery = new Parse.Query('Lead');
+  leadsQuery.equalTo('agent', req.user);
+  leadsQuery.find()
+    .then(leads => res.success(leads))
+    .catch(err => res.error(err));
+});
+
+/**
  * As an agent I want to update a Lead
  *
- * We query the database for Leads, using the Lead's id
- * If found, we check to see what is being updated, and set that update
+ * We query the database for Leads, using the Lead's id.
+ *
+ * If found, we check to see what is being updated, and set that update.
+ *
  * If the update is to add the Lead to a LeadGroup, we query for the
  * LeadGroup, and use addUnique to add the Lead to the array of pointers on the
- * LeadGroup, and add the LeadGroup to the array of pointers on the Lead.
+ * LeadGroup.  We add the LeadGroup to the array of pointers on the Lead.
+ * We save the LeadGroup.
+ *
  * Then we save the Lead.
  *
+ * @param  {object} lead An object representing the lead, including an id, name, phone, email, leadType, and leadGroup
  */
 
 Parse.Cloud.define('updateLead', (req, res) => {
@@ -80,12 +139,14 @@ Parse.Cloud.define('updateLead', (req, res) => {
 /**
  * As an agent I want to remove a LeadGroup from a Lead
  *
- * We query the database for Leads, using the Lead's id
+ * We query the database for Leads, using the Lead's id.
  * If found, we query for the LeadGroup, and use remove
  * to remove the Lead to the array of pointers on the
- * LeadGroup, and remoe the LeadGroup to the array of pointers on the Lead.
+ * LeadGroup, and remove the LeadGroup to the array of pointers on the Lead.
  * Then we save the LeadGroup.
  * Then we save the Lead.
+ *
+ * @param  {object} lead An object representing the lead, including an id, name, phone, email, leadType, and leadGroup
  *
  */
 
@@ -106,40 +167,23 @@ Parse.Cloud.define('removeGroupFromLead', (req, res) => {
     .catch(err => res.error(err));
 });
 
+
 /**
- * As an agent I want to fetch a Lead
- * We query the database for Lead
- * If found, we return the Lead & its associated leadGroups.
+ * As an agent I want to delete a Lead.
+ * We query the database for Lead.
  *
- */
-
-Parse.Cloud.define('fetchLead', (req, res) => {
-  const leadQuery = new Parse.Query('Lead');
-  leadQuery.include('leadGroups');
-  leadQuery.get(req.params.lead)
-    .then(lead => res.success(lead))
-    .catch(err => res.error(err));
-});
-
-/**
- * As an agent I want to fetch my Leads
- * We query the database for Leads
- * If found, we return the Leads
+ * If found, we query for leadGroups who have that lead in their array of leads.
+ * We then remove the Lead from each LeadGroup it is associated with.
+ * We save the LeadGroup.
  *
- */
-Parse.Cloud.define('fetchLeads', (req, res) => {
-  const leadsQuery = new Parse.Query('Lead');
-  leadsQuery.equalTo('agent', req.user);
-  leadsQuery.find()
-    .then(leads => res.success(leads))
-    .catch(err => res.error(err));
-});
-
-
-/**
- * As an agent I want to delete a Lead
- * We query the database for Lead
- * If found, we delete the Lead.
+ * Then we remove the lead from the database.
+ *
+ * Then we remove the lead from the Agent's array of leads.
+ * Then we save the Agent.
+ * We then query for the newly saved Agent and send it back with the success response.
+ *
+ *
+ * @param  {object} lead An object representing the lead, including an id, name, phone, email, leadType, and leadGroup
  *
  */
 
