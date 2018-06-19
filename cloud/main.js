d585ed01b00b8a573e8d909d67066c5520835706
Kayla Kantola <krkantola@gmail.com>
2018-06-19 12:09:28 -0400

    makes backend lead code functional

--- cloud/main.js
+++ cloud/main.js
@@ -100,6 +100,9 @@ Parse.Cloud.afterDelete(Parse.User, (req, res) => {
 
 const fetchUser = id => new Promise((resolve) => {
   const userQuery = new Parse.Query('User');
+  userQuery.include('agents');
+  userQuery.include('leads');
+  userQuery.include('leadGroups');
   resolve(userQuery.get(id, { useMasterKey: true }));
 });
 
